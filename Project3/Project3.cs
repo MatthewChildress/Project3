c561819a510912f34e72828d2370ac3a12986126
MatthewChildress <98126674+ArchdukeChilly@users.noreply.github.com>
2022-11-28 17:23:29 -0500

    uploading project 3

---
+++ Project3/Project3.cs
@@ -0,0 +1,50 @@
+﻿///<summary>
+///File: Project3.cs
+///Name: Matthew Childress
+///Class: CISP1010
+///Semester: Fall 2022
+///Project: Don't Be a Square, Be a Rectangle!
+/// </summary>
+namespace Project3
+{
+    internal class Project3
+    {
+        /// <summary>
+        /// The Main method is the entry point for this program
+        /// </summary>
+        static void Main()
+        {
+            // prints header
+            Console.WriteLine("Rectangle Calculator");
+            Console.WriteLine("**Enter q for quit**");
+            Console.WriteLine();
+
+            do
+            {
+                // declares variables
+                string input;
+                double width, height;
+
+                // prompts for width, checks that it's not q and converts to a double
+                Console.Write("width-> ");
+                input = Console.ReadLine();
+                if (input == "q") break;
+                width = Convert.ToDouble(input);
+
+                // prompts for height, checks that it's not q and converts to a double
+                Console.Write("height-> ");
+                input = Console.ReadLine();
+                if (input == "q") break;
+                height = Convert.ToDouble(input);
+
+                // instantiates new object
+                Rectangle rectangle = new Rectangle(width, height);
+
+                // prints rectangle properties
+                Console.WriteLine(rectangle);
+                Console.WriteLine();
+
+            } while (true);
+        }
+    }
+}
---
+++ Project3/Rectangle.cs
@@ -0,0 +1,91 @@
+﻿using System.Threading.Channels;
+///<summary>
+///File: Rectangle.cs
+///Name: Matthew Childress
+///Class: CISP1010
+///Semester: Fall 2022
+///Project: Don't Be a Square, Be a Rectangle!
+/// </summary>
+namespace Project3
+{
+    /// <summary>
+    /// Models a Rectangle object
+    /// </summary>
+    internal class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        /// <summary>
+        /// Parameterized constructor
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public Rectangle(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        /// <summary>
+        /// Calculates a rectangle's area from width and height
+        /// </summary>
+        public double Area
+        {
+
+            get
+            {
+                return Width * Height;
+            }
+
+        }
+        /// <summary>
+        /// Calculates a rectangle's diagonal length from width and height
+        /// </summary>
+        public double Diagional
+        {
+
+            get
+            {
+                return Math.Sqrt(Math.Pow(Width, 2) + Math.Pow(Height, 2));
+            }
+
+        }
+        /// <summary>
+        /// Checks if a rectangle is a square
+        /// </summary>
+        public bool IsSquare
+        {
+
+            get
+            {
+                return Width == Height;
+            }
+
+        }
+        /// <summary>
+        /// Calculates a rectangle's perimeter length from width and height
+        /// </summary>
+        public double Perimeter
+        {
+
+            get
+            {
+                return 2 * (Width + Height);
+            }
+
+        }
+        
+        
+        /// <summary>
+        /// Creates a string representation of the Rectangle
+        /// </summary>
+        /// <returns>The string representation of the Rectangle</returns>
+        override public string ToString()
+        {
+
+            return $"\nrectangle -> \nwidth = {Width} \nheight = {Height}\narea = {Area}\nperimeter = {Perimeter}\ndiagonal = {Diagional}\nis square = {IsSquare}";
+        }
+
+    }
+}
